1c792aa406807682d63582e421a7ca821119578b
DukTofn <ductoan110906@gmail.com>
2025-10-14 16:14:25 +0700

    Buoi 2

---
+++ Assets/GridDraw.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class GridDraw : MonoBehaviour {
+    public Vector2Int gridSize;
+    public Vector2 cellSize; 
+    public GameObject center;
+    public Vector3 anchor; 
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        AnchorUpdate();
+
+        for (int x = 0; x < gridSize.x; x++) {
+            for (int y = 0; y < gridSize.y; y++) {
+                Vector3 cellCenter = anchor + new Vector3(x * cellSize.x + cellSize.x / 2, y * cellSize.y + cellSize.y / 2, 0);
+
+                Vector3 cellSizee = new Vector3(cellSize.x, cellSize.y, 0);
+                Gizmos.DrawWireCube(cellCenter, cellSizee);
+            }
+        }
+    }
+
+    
+    private void AnchorUpdate() {
+        float totalWidth = gridSize.x * cellSize.x;
+        float totalHeight = gridSize.y * cellSize.y;
+        anchor = center.transform.position - new Vector3(totalWidth / 2, totalHeight / 2, 0);
+    }
+
+    private void OnMouseDown() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        AnchorUpdate();
+
+        Vector2Int gridPos = ConvertToGrid(mousePos);
+
+        Debug.Log($"Ô được click: {gridPos}");
+    }
+
+    private Vector2Int ConvertToGrid(Vector3 worldPos) {
+        Vector3 localPos = worldPos - anchor;
+
+        int col = Mathf.FloorToInt(localPos.x / cellSize.x);
+        int row = Mathf.FloorToInt(localPos.y / cellSize.y);
+
+        return new Vector2Int(col, row);
+    }
+}
